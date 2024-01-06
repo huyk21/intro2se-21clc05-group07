7a9664346deee90a241182acc16d4bf084eb3ccb
Quách Tấn Dũng <146900350+DunQua@users.noreply.github.com>
2024-01-06 20:00:18 +0700

    Create admin screen folder, and 2 files handle "orderList" and "productList" for admin

---
+++ src/front-end/src/screens/admin/orderlistscreen.jsx
@@ -0,0 +1,67 @@
+import { FaTimes } from "react-icons/fa";
+import Message from "../../components/message";
+import Loader from "../../components/loader";
+import { useGetOrdersQuery } from "../../slices/orderApiSlice";
+import { Table, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+const Order_List_Screen = () => {
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Paid</th>
+              <th>Delivered</th>
+              <th>Information</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders?.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    order.deliveredAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button className="btn-sm" variant="light">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default Order_List_Screen;
