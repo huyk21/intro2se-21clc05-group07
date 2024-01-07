5bcf288916ddfb00a0b7e1cd0bef2ad4c1f46286
myproject <buituankiet28112003@gmail.com>
2024-01-07 22:30:54 +0700

    quản lý trạng thái người dùng, giỏ hàng, đơn hàng, sản phẩm

---
+++ src/front-end/src/slices/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+//THIS IS ALL FOR LOCAL STORAGE
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+    logout: (state) => {
+      state.userInfo = null; //give it null
+      localStorage.removeItem("userInfo"); //remove from local storage
+    },
+  },
+});
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
---
+++ src/front-end/src/slices/productsApiSlice.js
@@ -0,0 +1,70 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCTS_URL,
+        params: { keyword, pageNumber },
+      }),
+      keepUnusedDataFor: 5,
+      provideTags: ["Products"],
+    }),
+    getProductDetails: builder.query({
+      query: (id) => ({
+        url: `${PRODUCTS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"], //
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.id}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    uploadImageProduct: builder.mutation({
+      query: (data) => ({
+        url: `/api/upload`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `${PRODUCTS_URL}/${id}`,
+        method: "DELETE",
+      }),
+    }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data._productId}/reviews`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    getTopRatedProducts: builder.query({
+      query: () => ({
+        url: `${PRODUCTS_URL}/top`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadImageProductMutation,
+  useDeleteProductMutation,
+  useCreateReviewMutation,
+  useGetTopRatedProductsQuery,
+} = productsApiSlice;
