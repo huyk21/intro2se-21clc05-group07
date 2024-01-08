fe981e5fee7696f9e901610d178ae7d51ed2fdf6
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-01-08 18:23:14 +0700

    finish server to handle backend

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -20,3 +20,32 @@ app.use(express.json()); //this will allow us to accept json data in the body
 app.use(express.urlencoded({ extended: true })); //this will allow us to accept form data in the body
 
 app.use(cookieParser()); //this will allow us to accept cookies in the body
+app.use("/api/users", userRoutes);
+
+app.use("/api/products", productRoutes);
+
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadImageRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
