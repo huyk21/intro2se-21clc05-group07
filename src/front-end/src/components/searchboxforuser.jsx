5d5758ded7a925b5e605d8cb6b095729d5ded338
TheWarm0nger <146900329+TheWarm0nger@users.noreply.github.com>
2024-01-08 21:59:55 +0700

    user search and more

---
+++ src/front-end/src/components/searchboxforuser.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+const SearchBoxForUser = () => {
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword || "");
+  const navigate = useNavigate();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    if (keyword) {
+      navigate(`/search/${keyword.trim()}`);
+      setKeyword("");
+    } else {
+      navigate("/");
+    }
+  };
+  return (
+    <Form onSubmit={submitFormHandler} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-success" className="p-2 mx-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
+export default SearchBoxForUser;
