23aba986eced816faf820401129b422883646343
Quách Tấn Dũng <146900350+DunQua@users.noreply.github.com>
2024-01-10 12:28:47 +0700

    Reword searchbox and update logo

--- src/front-end/src/components/searchboxforuser.jsx
+++ src/front-end/src/components/searchboxforuser.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import searchGlass from "../assets/search.png";
+
 const SearchBoxForUser = () => {
   const { keyword: urlKeyword } = useParams();
   const [keyword, setKeyword] = useState(urlKeyword || "");
@@ -16,20 +18,32 @@ const SearchBoxForUser = () => {
       navigate("/");
     }
   };
+
   return (
     <Form onSubmit={submitFormHandler} className="d-flex">
-      <Form.Control
-        type="text"
-        name="q"
-        onChange={(e) => setKeyword(e.target.value)}
-        value={keyword}
-        placeholder="Search Products..."
-        className="mr-sm-2 ml-sm-5"
-      ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2 mx-2">
-        Search
-      </Button>
+      <div className="input-group">
+        <Form.Control
+          type="text"
+          name="q"
+          onChange={(e) => setKeyword(e.target.value)}
+          value={keyword}
+          placeholder="Search product..."
+          className="mr-sm-2 ml-sm-5"
+          style={{ borderRadius: '25px' }}
+        />
+        <div className="input-group-append" style={{ position: 'absolute', right: '10px', top: '5px' }}>
+          <Button 
+            type="submit" 
+            className="p-1" 
+            title="Search" 
+            style={{ backgroundColor: '#3C4C5D', borderRadius: '30px', border: 'none' }}
+          >
+            <img src={searchGlass} alt="searchGlass" />
+          </Button>
+        </div>
+      </div>
     </Form>
   );
 };
+
 export default SearchBoxForUser;
