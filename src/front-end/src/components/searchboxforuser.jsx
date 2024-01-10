30ab84d850cd84e2b9c5ed4e64e13908e8f5cf47
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-01-10 12:41:37 +0700

    fixed error img url not in right folder assests

--- src/front-end/src/components/searchboxforuser.jsx
+++ src/front-end/src/components/searchboxforuser.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import searchGlass from "../assets/search.png";
+import search from "../assets/search.png";
 
 const SearchBoxForUser = () => {
   const { keyword: urlKeyword } = useParams();
@@ -29,16 +29,23 @@ const SearchBoxForUser = () => {
           value={keyword}
           placeholder="Search product..."
           className="mr-sm-2 ml-sm-5"
-          style={{ borderRadius: '25px' }}
+          style={{ borderRadius: "25px" }}
         />
-        <div className="input-group-append" style={{ position: 'absolute', right: '10px', top: '5px' }}>
-          <Button 
-            type="submit" 
-            className="p-1" 
-            title="Search" 
-            style={{ backgroundColor: '#3C4C5D', borderRadius: '30px', border: 'none' }}
+        <div
+          className="input-group-append"
+          style={{ position: "absolute", right: "10px", top: "5px" }}
+        >
+          <Button
+            type="submit"
+            className="p-1"
+            title="Search"
+            style={{
+              backgroundColor: "#3C4C5D",
+              borderRadius: "30px",
+              border: "none",
+            }}
           >
-            <img src={searchGlass} alt="searchGlass" />
+            <img src={search} alt="searchGlass" />
           </Button>
         </div>
       </div>
